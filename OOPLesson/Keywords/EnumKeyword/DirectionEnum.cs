dca62a98f366422ad9d90a65fe05d3a1c450f247
Faruk Aydin <farukydn97@gmail.com>
2021-10-13 16:33:22 +0300

    Added enum and struct

---
+++ OOPLesson/Keywords/EnumKeyword/DirectionEnum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPLesson.Keywords.EnumKeyword
+{
+    // Int türünde değer tutarlar
+    // Default olarak N = 0, E = 1, S = 2 olarak atanır
+    // Yalnızca tamsayı türlerinden extend edilebilir. Bu enum'ın value'sinin türünü belirler
+    // Örneğin DirectionEnum int türünde değer tutar (Default olarak int atar)
+    // ToString("D") ile int türündeki değeri alınabilir
+    enum DirectionEnum : int
+    {
+        N,
+        E,
+        S,
+        W = 3,
+
+        // Enumlar method barındıramaz
+        public void SomeMethod()
+        {
+
+        }
+    }
+}
---
+++ OOPLesson/Keywords/StructKeyword/MyStruct.cs
@@ -0,0 +1,27 @@
+﻿namespace OOPLesson.StructKeyword
+{
+    // Stackte tutuluyor, yani value type değişkenler
+    // 16 byte olmalı maksimum
+    // Boş constructor yazılamaz
+    // Struct constructor içinde tüm parametreler set edilmelidir
+    // Extend edilemez, implemente edilebilir
+    public struct MyStruct : IMyInterface
+    {
+        public int Value { get; set; }
+
+        public MyStruct(int value)
+        {
+            Value = value;
+        }
+
+        public void AddTen()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IMyInterface
+    {
+        public void AddTen();
+    }
+}
